f21681dc556f6ddc50728cff99a0f2db89baeb81
Andrew Horvath <skwerl@student.fullsail.edu>
2022-11-23 15:25:33 -0600

    adding more routes and dynamic routing

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('Home');
+});
+
+app.get('/products', (req, res) => {
+    res.send('All Products');
 })
+
+app.post('/products', (req, res) => {
+    res.send('Created a New Product');
+});
+
+app.post('/products/:id', (req, res) => {
+    res.send('Updated a Product with and id of: ' + req.params.id);
+});
 
 const port = 3000;
 app.listen(port, () => {
