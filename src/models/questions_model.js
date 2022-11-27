49a42b4317fe6c7fe8f5a55a212f7aceea8c7057
Andrew Horvath <skwerl@student.fullsail.edu>
2022-11-27 14:48:42 -0600

    added questions_model.js

---
+++ src/models/questions_model.js
@@ -0,0 +1,8 @@
+let questions = [
+    { id: 1, name: 'What is this Question One?'},
+    { id: 2, name: 'What is that Question Two?'},
+    { id: 3, name: 'Where is the Question Three?'},
+    
+];
+
+module.exports = questions;
