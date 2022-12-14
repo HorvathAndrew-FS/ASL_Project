7f28f07d51f5482138b99bde3e8533d192ebe4c1
Andrew Horvath <skwerl@student.fullsail.edu>
2022-12-14 09:39:42 -0600

    getting started with react

--- asl-react-app/src/App.js
+++ asl-react-app/src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes
+} from 'react-router-dom';
+import Navigation from './Navigation'
+import Home from './Home'
+import Login from './Login'
+import Quiz from './Quiz'
+import queryString from 'querystring';
 
-function App() {
+const App = () => {
+  const [gitToken, setGitToken] = useState('')
+  useEffect(() => {
+    async function fetchGitToken() {
+      const params = queryString.parse(window.location.search.replace(/^\?/, ''))
+      localStorage.token = params.token
+      const response = await axios('http://localhost:3000/auth/token/', {
+        headers: {
+          token: localStorage.token
+        }
+      })
+      setGitToken(response.data.token)
+    }
+    fetchGitToken()
+  }, []);
+
+  if (!gitToken) {
+    return <Login />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation isLoggedIn={gitToken ? true : false} />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/quizzes/:id' element={<Quiz />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
